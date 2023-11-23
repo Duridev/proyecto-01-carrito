10bcdec7c7d71f738b987c230c7d4ad99159d804
duridev <duridev@outlook.com>
2023-11-23 10:53:12 -0300

    leer datos de curso seleccionado

--- js/app.js
+++ js/app.js
@@ -18,6 +18,26 @@ function cargarEventListeners() {
 
 function agregarCurso(e) {
     e.preventDefault();
-    if (e.target.classList.contains('agregar-carrito'))
-    console.log('Agregando al carrito');
+
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado)
+
+    }
 };
+
+// Lee el contenido del HTML al que le dimos click y extrae la información del curso
+
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    //crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('precio').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+}
